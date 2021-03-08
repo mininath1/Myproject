794b96c0443277fde88af8e5a57cd33c970460af
Mininath Mahanavar <mmininath1@gmail.com>
2021-03-08 08:53:14 +0530

    added on click listner

--- MySampleProject/app/src/main/java/com/example/mysampleproject/MainActivity.java
+++ MySampleProject/app/src/main/java/com/example/mysampleproject/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.mysampleproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "button clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
